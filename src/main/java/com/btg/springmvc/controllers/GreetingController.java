1138f129b8c2e2304197c4f50779a80df3336694
Matthew Borders <mborders@outlook.com>
2017-11-16 00:28:53 -0600

    Added form example

--- src/main/java/com/btg/springmvc/controllers/GreetingController.java
+++ src/main/java/com/btg/springmvc/controllers/GreetingController.java
@@ -1,7 +1,11 @@
 package com.btg.springmvc.controllers;
 
+import com.btg.springmvc.models.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,19 +15,14 @@ import java.util.List;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public String greeting(Model model) {
-        model.addAttribute("name", "Bob");
-        model.addAttribute("age", 23);
-
-        List<String> days = new ArrayList<>();
-        days.add("Monday");
-        days.add("Tuesday");
-        days.add("Wednesday");
-
-        model.addAttribute("days", days);
-
+        model.addAttribute("greeting", new Greeting());
         return "greeting";
     }
 
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "greeting";
+    }
 }
--- src/main/java/com/btg/springmvc/controllers/HomeController.java
+++ src/main/java/com/btg/springmvc/controllers/HomeController.java
@@ -2,13 +2,14 @@ package com.btg.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
 
 @Controller
 public class HomeController {
-  @RequestMapping("/")
+  @GetMapping("/")
   public String home(Model model) {
     model.addAttribute("time", new Date());
     return "home";
